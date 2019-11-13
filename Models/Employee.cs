d5c3f5df422e79afe36f5756ddf333782738d669
ashiqqa <ashiq.mail.net@gmail.com>
2019-11-13 22:44:09 +0600

    delegate practice

---
+++ Models/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeAdvanceCS.Models
+{
+    public delegate bool SeniorChecker(Employee employee);
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public int Experience { get; set; }
+
+        public static void GetFilteredEmployee(List<Employee> employess, SeniorChecker seniorChecker)
+        {
+            foreach (Employee emp in employess)
+            {
+                if (seniorChecker(emp))
+                {
+                    Console.WriteLine($"Name:{emp.Name}Experience: {emp.Experience} Salary: {emp.Salary}");
+                }
+            }
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using PracticeAdvanceCS.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeAdvanceCS
+{
+    //Inner Exception
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                try
+                {
+                    Console.WriteLine("Number-1: ");
+                    int num1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Number-2: ");
+                    int num2 = Convert.ToInt32(Console.ReadLine());
+                    int devideResult = num1 / num2;
+                    Console.WriteLine(devideResult);
+                }
+                catch (Exception ex)
+                {
+                    string filePath = @"I:\PracticeBasicCS\Notesjk\ExceptionLog.txt";
+                    if (File.Exists(filePath))
+                    {
+                        StreamWriter streamWrite = new StreamWriter(filePath);
+                        streamWrite.Write(ex.GetType().Name);
+                        streamWrite.WriteLine();
+                        streamWrite.Write(ex.Message);
+                        streamWrite.WriteLine();
+                        streamWrite.Write(ex.StackTrace);
+                        streamWrite.Close();
+                    }
+                    else
+                    {
+                        throw new Exception($"{filePath} not exist!", ex);
+                    }
+                }
+            }
+            catch(Exception ex2)
+            {
+                Console.WriteLine($"Current Exception : {ex2.Message}");
+                if (ex2.InnerException != null)
+                {
+                    Console.WriteLine($"Inner Exception : {ex2.InnerException}");
+                }
+            }
+
+
+            Console.ReadKey();
+        }
+
+    }
+}
