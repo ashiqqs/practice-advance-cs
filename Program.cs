928dea00e4e06a4a49789916d5d1a8aa59287aef
ashiqqa <ashiq.mail.net@gmail.com>
2019-11-16 23:49:29 +0600

    access modifier

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using PracticeAdvanceCS.Models;
+﻿using AssemblyOne;
+using AssemblyTwo;
+using PracticeAdvanceCS.Models;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -9,35 +11,14 @@ using System.Threading.Tasks;
 
 namespace PracticeAdvanceCS
 {
-    //Enum
     class Program
     {
         public static void Main(string[] args)
         {
-            List<Employee> employees = new List<Employee>()
-            {
-                new Employee{Name = "Mark",Gender = Gender.Male},
-                new Employee{Name = "Mery",Gender = Gender.Female},
-                new Employee{Name = "Sam",Gender = Gender.Unknown}
-            };
-
-            foreach(var emp in employees)
-            {
-                Console.WriteLine($"Name {emp.Name}, Gender: {emp.Gender}, GenderId: {(int)emp.Gender}");
-            }
+            AssemblyTwoClassOne ATCO = new AssemblyTwoClassOne();
+            Console.WriteLine(ATCO.GetProtectedInternal());
             Console.ReadKey();
         }
     }
-    public class Employee
-    {
-        public string Name { get; set; }
-        public Gender Gender { get; set; }
-    }
 
-    public enum Gender
-    {
-        Male=1,
-        Female=2,
-        Unknown=0
-    }
 }
